ea9ef9e020ad2cac3d44dd6b5c4745922dd5c55c
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-05-16 10:09:40 -0700

    Adjusted for local dev env

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 /* global d3 */
 
+// use remote file on codepen
 // d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json', (error, data) => {
 d3.json('../temp/cyclist-data.json', (error, data) => {
   if (error) {
