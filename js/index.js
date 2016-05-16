e9ffd0ecc823160d343d5fb2bea0721a8bf52dec
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-05-16 09:37:40 -0700

    Added local copy for dev

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 /* global d3 */
+
+// d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json', (error, data) => {
+d3.json('../temp/cyclist-data.json', (error, data) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(data);
+});
